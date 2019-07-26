8a355b11d598b7197265360ca891e05269b73ee2
Nate Jones <njones@rstudio03.research.sesync.org>
2019-07-26 13:43:02 -0400

    watershed delineation

--- RScripts/AIK_WetlandMetrics.R
+++ RScripts/AIK_WetlandMetrics.R
@@ -20,13 +20,12 @@ rm(list=ls(all=TRUE))
 data_dir<-"//nfs/palmer-group-data/choptank/Nate/Storage_Capacity/"
 
 #Download packages 
-library(tidyverse)
-library(raster)
-library(sf)
-library(parallel)
-library(whitebox)
-library(fasterize)
 library(stars)
+library(fasterize)
+library(whitebox)
+library(sf)
+library(raster)
+library(tidyverse)
 
 #Load custom R scripts
 funs<-list.files("functions/")
@@ -104,17 +103,16 @@ clump(input = paste0(data_dir, "II_Work/reclass_na.tif"),
       zero_back = T)
 
 #Identify Clusters greater than 100m2
-c<-raster(paste0(data_dir, "II_Work/group.tif")) 
-c[c==1]<-NA
-c<- c %>% 
+giws<-raster(paste0(data_dir, "II_Work/group.tif")) 
+giws[giws==1]<-NA
+giws<- giws %>% 
   #Convert to polygon
   st_as_stars(.) %>% st_as_sf(., merge = TRUE) %>%
   #Estiamte polygon areas
   mutate(area = as.numeric(st_area(., by_id=T))) %>%
   #filter small areas out
   filter(area>100) 
-st_write(c, paste0(data_dir, "II_Work/giws_lr.shp"), delete_layer=TRUE)
-remove(c)
+st_write(giws, paste0(data_dir, "II_Work/giws_lr.shp"), delete_layer=TRUE)
 
 #2.4 Create low resolution fdr and fac------------------------------------------
 #Breach depressions
@@ -130,131 +128,158 @@ d8_flow_accumulation(dem = paste0(data_dir,"II_Work/dem_breach.tif"),
                      output = paste0(data_dir,"II_Work/fac_lr.tif"))
 
 #import into R workspace
-burn<-raster(paste0(data_dir,"II_Work/dem_burn.tif"))
-breach<-raster(paste0(data_dir,"II_Work/dem_breach.tif"))
 fdr<-raster(paste0(data_dir,"II_Work/fdr_lr.tif"))
 fac<-raster(paste0(data_dir,"II_Work/fac_lr.tif"))
 
 #3.0 Delineate Watersheds-------------------------------------------------------
-#Create function to delineate watershed
-#fun<-function(){}
+#3.1 Create function to delineate watershed-------------------------------------
+fun<-function(n=1, #wetland of interest in pnts dataframe
+             pnts , #pnts marking wetlands of interests
+             giws , #wetlands shp derived from DEM [and informed by manual burn shps]){}
+             fac , #flow accumulation raster (low res)
+             fdr , #flow direction raster (low res)
+             dem_hr=dem){ #hi resolution dem
 
-#Create temporary file
-temp_dir<-paste0(tempfile(),"/")
-dir.create(temp_dir)
-
-
-
-
-
-
-
-
-
-
-#3.0 Delineate Wetlands---------------------------------------------------------
-#Create function to identify depressions
-fun<-function(dem=dem,
-              min_size=5000, #minimum area (in map units)
-              iterations=100, #number of Monte Carlo iterations
-              dem_rmse=0.0607){ #RMSE of 18.5 cm
-
-#Create temporary file
-temp_dir<-paste0(tempfile(),"/")
-dir.create(temp_dir)
+  #Steps
+  # (1) Create temp directory
+  # (2) Delineate watershed with low res DEM
+  # (3) Crop high res DEM to "low res" watershed delineation
+  # (4) Re-delineate watershed with high res data
   
-#Write dem to scratch workspace
-writeRaster(dem, paste0(temp_dir, "dem.tif"), overwrite=T)
-
-#Fill single-cell depressions
-fill_single_cell_pits(dem    = paste0(temp_dir, "dem.tif"), 
-                      output = paste0(temp_dir,"dem_fill.tif"))
-
-#Filter the DEM
-edge_preserving_mean_filter(input  = paste0(temp_dir,"dem_fill.tif"), 
-                            output = paste0(temp_dir,"dem_filter.tif"), 
-                            threshold = 100)
-
-#Identify depressions
-stochastic_depression_analysis(dem = paste0(temp_dir,"dem_filter.tif"), 
-                               output = paste0(temp_dir,"depression.tif"), 
-                               rmse = dem_rmse,
-                               range = 100, 
-                               iterations =iterations)
-
-#Reclass raster (any depression that was delineated less than 80% of the time is out!)
-reclass(input = paste0(temp_dir,"depression.tif"), 
-        output = paste0(temp_dir,"reclass.tif"), 
-        reclass_vals = "'0;0;0.8'")
-reclass(input = paste0(temp_dir,"reclass.tif"), 
-        output = paste0(temp_dir,"reclass.tif"), 
-        reclass_vals = "'1;0.8;1'")
-r<-raster(paste0(temp_dir,"reclass.tif")) 
-r[r==0]<-NA
-writeRaster(r, paste0(temp_dir, "reclass_na.tif"), overwrite=T)
-
-#Identify "clumps" of depressions
-clump(input = paste0(temp_dir,"reclass_na.tif"), 
-      output = paste0(temp_dir,"group.tif"), 
-      zero_back = T)
-
-#Identify Clusters greater than 100m2
-w<-raster(paste0(temp_dir,"group.tif")) 
-w[w==1]<-NA
-w<- w %>% 
-  #Convert to polygon
-  st_as_stars(.) %>% st_as_sf(., merge = TRUE) %>%
-  #Estiamte polygon areas
-  mutate(area = as.numeric(st_area(., by_id=T))) %>%
-  #filter small areas out
-  filter(area>min_size) 
-
-#Export
-w
+  #3.1.1 Create temporary dir~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  scratch_dir<-paste0(tempfile(),"/")
+  dir.create(scratch_dir)
+  
+  #3.1.2 Delineate watershed with low res DEM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #Identify GIW of interest
+  pnt<-pnts[n,]
+  giw<-giws[pnt,]
+  
+  #Add 3 m buffer (10 m comes from the resolutin of the raster)
+  giw<-st_buffer(giw, 10/3)
+  
+  #Find max fac point within giw
+  fac_giw<-crop(fac, as_Spatial(giw))
+  fac_giw<-mask(fac_giw, as_Spatial(giw))
+  
+  #create pour point
+  pp<-rasterToPoints(fac_giw) %>% 
+    #convert to tibble
+    as_tibble(.) %>%
+    #filter to just max fac value
+    filter(fac_lr == base::max(fac_lr, na.rm=T)) %>%
+    #Select first row
+    slice(1)
+  
+  #Make pour point an sf shape
+  pp<-st_as_sf(pp, coords = c("x", "y"), crs = st_crs(giw))
+  
+  #Export files to scratch dir
+  writeRaster(fac, paste0(scratch_dir, "fac.tif"))
+  writeRaster(fdr, paste0(scratch_dir, "fdr.tif"))
+  write_sf(pp, paste0(scratch_dir,"pp.shp"), delete_layer = T)
+  
+  #Delineate Watershed w/ WBT
+  watershed(d8_pntr = paste0(scratch_dir, "fdr.tif"), 
+            pour_pts = paste0(scratch_dir,"pp.shp"), 
+            output = paste0(scratch_dir,"watershed.tif"))
+  
+  #Read watershed in
+  w_grd<-raster(paste0(scratch_dir,"watershed.tif"))
+  
+  #Convert watershed to polygon
+  w_shp<- w_grd %>% st_as_stars() %>% st_as_sf(., merge = TRUE)
+  
+  #Export watershed shp
+  write_sf(w_shp, paste0(data_dir, "II_Work/",pnt$wetland,"_low_res_watershed.shp"), delete_layer = T)
+  
+  #3.1.3 Re-delineate watershed with high res data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #Clip high resolution DEM with w_shp
+  mask<-st_buffer(w_shp, 100)
+  dem_hr<-crop(dem_hr, as_Spatial(mask))
+  
+  #Write raster to scratch workspace
+  writeRaster(dem_hr,paste0(scratch_dir,"dem_hr.tif"), overwrite=T)
+  
+  #Breach Analysis of the DEM
+  breach_depressions(dem = paste0(scratch_dir,"dem_hr.tif"), 
+                     output = paste0(scratch_dir,"dem_breach.tif"), 
+                     fill_pits = TRUE)
+  
+  #Flow Direction and flow accumulation
+  d8_pointer(dem = paste0(scratch_dir,"dem_breach.tif"), 
+             output = paste0(scratch_dir,"fdr_hr.tif"))
+  d8_flow_accumulation(dem = paste0(scratch_dir,"dem_breach.tif"), 
+                       output = paste0(scratch_dir,"fac_hr.tif"))
+  
+  #Read fac and fdr into R environment
+  fac_hr<-raster(paste0(scratch_dir,"fac_hr.tif"))
+  fdr_hr<-raster(paste0(scratch_dir,"fdr_hr.tif"))
+  
+  #Find max fac point within giw
+  fac_giw_hr<-crop(fac_hr, as_Spatial(giw))
+  fac_giw_hr<-mask(fac_giw_hr, as_Spatial(giw))
+  
+  #create pour point
+  pp_hr<-rasterToPoints(fac_giw_hr) %>% 
+    #convert to tibble
+    as_tibble() %>%
+    #select point with max fac
+    filter(fac_hr==base::max(fac_hr))
+  
+  #Make pour point an sf shape
+  pp_hr<-st_as_sf(pp_hr, coords = c("x", "y"), crs = st_crs(giw))
+  
+  #Export pour point to scratch workspace
+  write_sf(pp_hr, paste0(scratch_dir,"pp_hr.shp"), delete_layer = T)
+  
+  #Delineate Watershed w/ WBT
+  watershed(d8_pntr = paste0(scratch_dir, "fdr_hr.tif"), 
+            pour_pts = paste0(scratch_dir,"pp_hr.shp"), 
+            output = paste0(scratch_dir,"watershed_hr.tif"))
+  
+  #Read watershed in
+  w_hr_grd<-raster(paste0(scratch_dir,"watershed_hr.tif"))
+  
+  #Convert watershed to polygon
+  w_hr_shp<- w_hr_grd %>% st_as_stars() %>% st_as_sf(., merge = TRUE)
+  
+  #Add id info and write to output folder
+  write_sf(w_hr_shp, paste0(data_dir, "II_Work/",pnt$wetland,"_high_res_watershed.shp"), delete_layer = T)
 }
 
-#Apply function 
-giws_jl<-fun(dem=dem_burn_jl)
-giws_jr<-fun(dem=dem_burn_jr)
+#3.2 Apply function-------------------------------------------------------------
+#Run function [use loop for error tracking]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+for(i in 1:nrow(pnts)){
+  print(i)
+  fun(n=i, 
+      pnts = pnts,
+      giws = giws, 
+      fac = fac, 
+      fdr = fdr, 
+      dem_hr=dem)
+}
 
-#combine giws
-giws<-rbind(giws_jl, giws_jr)
+#Gather shapes and export~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Make list of watershed shape files
+watersheds<-list.files(paste0(data_dir,"II_work")) %>%
+  enframe() %>%
+  select(value) %>%
+  filter(str_detect(value, "high_res_watershed.shp"))
 
-#export to data directory
-st_write(giws_jl, paste0(data_dir, "II_Work/giws_jl.shp"))
-st_write(giws_jr, paste0(data_dir, "II_Work/giws_jr.shp"))
-st_write(giws,    paste0(data_dir, "II_Work/giws.shp"))
+#load those shapes
+watersheds_shp<-st_read(paste0(data_dir,"II_work/", watersheds[1,]))
+watersheds_shp$ID<-substr(watersheds$value[1],1,2)
+for(i in 2:nrow(watersheds)){
+  temp_shp<-st_read(paste0(data_dir,"II_work/", watersheds[i,]))
+  temp_shp$ID<-substr(watersheds$value[i],1,2)
+  watersheds_shp<-rbind(watersheds_shp, temp_shp)
+}
 
-#4.0 Delinate Watersheds--------------------------------------------------------
-#Create function to delineate watershed
-#fun<-function(n, 
-#              pnts = pnts, 
-#              giws = giws){}
+#export 
+st_write(watersheds_shp, paste0(data_dir,"III_Products/watersheds.shp"))
 
-#For testing
-n<-1
-
-#Isolate wetland of interests
-giw<-giws[pnts[n,],]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#3.2 Wetland Subshed Delineation------------------------------------------------
+#4.0 Wetland Subshed Delineation------------------------------------------------
 #Create rapper function for subshed delineation
 fun<-function(WetID){
   #Define point of interest
